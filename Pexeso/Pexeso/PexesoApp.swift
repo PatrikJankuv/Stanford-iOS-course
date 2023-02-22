3a793de331b8b3a50b97068ce3c17bce7df71048
Patrik Jankuv <jankuv.patri@outlook.com>
2023-02-23 00:27:21 +0100

    Pexeso - lesson 2

---
+++ Pexeso/Pexeso/PexesoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexesoApp.swift
+//  Pexeso
+//
+//  Created by Patrik Jankuv on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PexesoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
