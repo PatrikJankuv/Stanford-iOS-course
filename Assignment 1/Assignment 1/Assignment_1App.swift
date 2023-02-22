482c295f51eb257105d279c07037ef5764ec822a
Patrik Jankuv <jankuv.patri@outlook.com>
2023-02-23 00:45:58 +0100

    Create Assignment 1

---
+++ Assignment 1/Assignment 1/Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1App.swift
+//  Assignment 1
+//
+//  Created by Patrik Jankuv on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
